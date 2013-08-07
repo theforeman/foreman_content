075b3c44872acb65c70a85cea1b86ca5cddbfe4d
Amos Benari <abenari@redhat.com>
2013-08-07 16:57:51 +0300

    updated missing relation

--- app/models/content/content_view.rb
+++ app/models/content/content_view.rb
@@ -1,8 +1,8 @@
 module Content
   class ContentView < ActiveRecord::Base
-    has_many :content_view_availabilities, :dependent => :destroy
-    belongs_to :operatingsystem, :through => :content_view_availabilities
-    has_many :hostgroups, :through => :content_view_availabilities
+    has_many :available_content_views, :dependent => :destroy
+    has_many :hostgroup, :through => :available_content_views
 
+    delegate :operatingsystem, :to => :available_content_views, :allow_nil => true
   end
 end
