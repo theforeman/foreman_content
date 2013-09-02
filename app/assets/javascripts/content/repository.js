86b98726ba50a045b6ffd781bda5ef63c684900a
Ohad Levy <ohadlevy@redhat.com>
2013-09-02 14:45:22 +0300

    try to guess the arch based on the repo url

--- app/assets/javascripts/content/repository.js
+++ app/assets/javascripts/content/repository.js
@@ -3,5 +3,17 @@ $(function () {
   $('#content_repository_schedule').click(function () {
     $('#sync_schedule_form select').attr('disabled', !$(this).is(':checked'));
   });
+  // try to guess architecture based on the URL
+  $('#content_repository_feed').focusout(function() {
+    url = $(this).val();
+    if (url == undefined || url == '')
+      return;
 
+    $('#content_repository_architecture_id option').each (function ( index, option) {
+      if (url.match(option.text)) {
+        $('#content_repository_architecture_id').val(option.value);
+        return false;
+      }
+    })
+  });
 });
